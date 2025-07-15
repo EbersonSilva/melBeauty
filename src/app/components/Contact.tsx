11ec979766041da0930c7940271aaba6d7764751
Luiz Zanellatto <luiz.zanellatto@pacpoapagg.com>
2025-07-15 16:53:49 -0300

    build criado

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,17 +1,17 @@
-export const Contact = () => {
-    return(
-        <section id="contato" className="py-20 bg-pink-100">
-        <div className="max-w-7xl mx-auto text-center">
-          <h2 className="text-3xl font-bold text-pink-600">Entre em Contato</h2>
-          <div className="mt-10">
-            <form action="#" method="POST" className="max-w-2xl mx-auto space-y-4">
-              <input type="text" name="nome" placeholder="Seu nome" className="w-full p-4 rounded-lg border border-gray-300" />
-              <input type="email" name="email" placeholder="Seu e-mail" className="w-full p-4 rounded-lg border border-gray-300" />
-              <textarea name="mensagem" placeholder="Sua mensagem" className="w-full p-4 rounded-lg border border-gray-300" rows="4"></textarea>
-              <button type="submit" className="w-full bg-pink-600 text-white p-4 rounded-lg">Enviar</button>
-            </form>
-          </div>
-        </div>
-      </section>
-    )
-}
+// export const Contact = () => {
+//     return(
+//         <section id="contato" className="py-20 bg-pink-100">
+//         <div className="max-w-7xl mx-auto text-center">
+//           <h2 className="text-3xl font-bold text-pink-600">Entre em Contato</h2>
+//           <div className="mt-10">
+//             <form action="#" method="POST" className="max-w-2xl mx-auto space-y-4">
+//               <input type="text" name="nome" placeholder="Seu nome" className="w-full p-4 rounded-lg border border-gray-300" />
+//               <input type="email" name="email" placeholder="Seu e-mail" className="w-full p-4 rounded-lg border border-gray-300" />
+//               <textarea name="mensagem" placeholder="Sua mensagem" className="w-full p-4 rounded-lg border border-gray-300" rows="4"></textarea>
+//               <button type="submit" className="w-full bg-pink-600 text-white p-4 rounded-lg">Enviar</button>
+//             </form>
+//           </div>
+//         </div>
+//       </section>
+//     )
+// }
