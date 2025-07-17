5393ae2fea89e481bccab45d726f9ecf6b81c748
Eberson Silva <binho.ebs@gmail.com>
2025-07-16 22:05:52 -0300

    Atualização imagens da Galeria

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,23 +1,45 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Lightbox from 'yet-another-react-lightbox';
-import Zoom from 'yet-another-react-lightbox/plugins/zoom';
-import Image from 'next/image';
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Image from "next/image";
 
-import 'yet-another-react-lightbox/styles.css';
+import "yet-another-react-lightbox/styles.css";
 // import 'yet-another-react-lightbox/plugins/zoom.css';
-
 
 export const Gallery = () => {
   const images = [
-    { src: '/images/eu.jpg', alt: 'Make 1' },
-    { src: '/images/eu.jpg', alt: 'Make 2' },
-    { src: '/images/eu.jpg', alt: 'Make 3' },
-    { src: '/images/eu.jpg', alt: 'Make 4' },
-    { src: '/images/eu.jpg', alt: 'Make 5' },
-    { src: '/images/eu.jpg', alt: 'Make 5' },
-
+    {
+      src: "/images/trabalhos/make (1).jpeg",
+      alt: "Make 1",
+      description: "Volume brasileiro.",
+    },
+    {
+      src: "/images/trabalhos/make (2).jpeg",
+      alt: "Make 2",
+      description: "Maquiagem noiva.",
+    },
+    {
+      src: "/images/trabalhos/make (3).jpeg",
+      alt: "Make 3",
+      description: "Design com Henna.",
+    },
+    {
+      src: "/images/trabalhos/make (4).jpeg",
+      alt: "Make 4",
+      description: "Volume fox eyes.",
+    },
+    {
+      src: "/images/trabalhos/make (5).jpeg",
+      alt: "Make 5",
+      description: "Maquiagem para debutante.",
+    },
+    {
+      src: "/images/trabalhos/make (6).jpeg",
+      alt: "Make 6",
+      description: "Maquigem social.",
+    },
   ];
 
   const [index, setIndex] = useState(-1);
@@ -41,6 +63,7 @@ export const Gallery = () => {
                 height={300}
                 className="rounded-lg object-cover w-full h-64 hover:scale-105 transition-transform duration-300"
               />
+              <p className="text-md text-black mt-1">{img.description}</p>
             </button>
           ))}
         </div>
@@ -51,12 +74,12 @@ export const Gallery = () => {
           index={index}
           slides={images.map((img) => ({ src: img.src }))}
           plugins={[Zoom]}
-           zoom={{
-    maxZoomPixelRatio: 3, // aumenta a capacidade de zoom
-    scrollToZoom: true,    // ativa zoom com scroll no desktop
-    doubleTapDelay: 300,   // ativa zoom por clique rápido duplo
-    zoomInMultiplier: 2,   // controle de zoom nos botões
-  }}
+          zoom={{
+            maxZoomPixelRatio: 3, // aumenta a capacidade de zoom
+            scrollToZoom: true, // ativa zoom com scroll no desktop
+            doubleTapDelay: 300, // ativa zoom por clique rápido duplo
+            zoomInMultiplier: 2, // controle de zoom nos botões
+          }}
         />
       </div>
     </section>
